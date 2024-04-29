74ef604c354e7206dc8f319ff76d7def01b99e85
Rob Drouin <rob.drouin@gmail.com>
2024-04-29 18:38:33 -0400

    Troubleshooting Point

--- src/NewsReader.js
+++ src/NewsReader.js
@@ -136,6 +136,7 @@ export function NewsReader() {
           <div className="box">
             <span className='title'>Query Form</span>
             <QueryForm
+              currentUser={currentUser}
               setFormObject={setQueryFormObject}
               formObject={queryFormObject}
               submitToParent={onFormSubmit}
--- src/QueryForm.js
+++ src/QueryForm.js
@@ -1,40 +1,112 @@
+import React from "react";
+
 export function QueryForm(params) {
+  const handleChange = (event) => {
+    let newQueryObject = { ...params.formObject };
+    newQueryObject[event.target.name] = event.target.value;
+    params.setFormObject(newQueryObject);
+  };
 
-    const handleChange = (event) => {
-        let newQueryObject = { ...params.formObject };
-        newQueryObject[event.target.name] = event.target.value;
-        params.setFormObject(newQueryObject);
-    };
+  function onSubmitClick(event) {
+    event.preventDefault();
+    if (!params.formObject.queryName) {
+      alert("Please provide a name for the query!");
+      return;
+    }
+    if (!params.formObject.q || params.formObject.q.length === 0) {
+      alert("Please provide some text for the query field!");
+      return;
+    }
+    params.submitToParent(params.formObject);
+  }
 
-    function onSubmitClick(event) {
-        event.preventDefault();
-        if (!params.formObject.queryName) {
-            alert("please provide a name for the query!");
-            return;
-        }
-        if (!params.formObject.q || params.formObject.q.length === 0) {
-            alert("please provide some text for the query field!");
-            return;
-        }
-        params.submitToParent(params.formObject);
-    };
+  function currentUserIsAdmin() {
+    if (params.currentUser && params.currentUser.user === "admin") {
+      return true;
+    }
+    return false;
+  }
 
-    return (
+  return (
+    <div>
+      <form>
         <div>
-            <form>
-                <div>
-                    <label htmlFor="queryName">Query Name: </label>
-                    <input type="text" size={10} id="queryName" name="queryName" value={params.formObject.queryName} onChange={handleChange} />
-                </div>
-                <div>
-                    <label htmlFor="q">Query Text: </label>
-                    <input type="text" size={10} id="q" name="q" value={params.formObject.q} onChange={handleChange} />
-                </div>
-                <span style={{ display: "block", backgroundColor: "#eee" }}>
-                    <input type="button" value="Submit" onClick={onSubmitClick} />
-                </span>                
-            </form>
+          <label htmlFor="queryName">Query Name: </label>
+          <input
+            type="text"
+            size={10}
+            id="queryName"
+            name="queryName"
+            value={params.formObject.queryName}
+            onChange={handleChange}
+          />
         </div>
-    );
-
+        <div>
+          <label htmlFor="q">Query Text: </label>
+          <input
+            type="text"
+            size={10}
+            id="q"
+            name="q"
+            value={params.formObject.q}
+            onChange={handleChange}
+          />
+        </div>
+        {currentUserIsAdmin() && (
+          <div>
+            <div>
+              <label htmlFor="language">Language: </label>
+              <input
+                type="text"
+                size={10}
+                id="language"
+                name="language"
+                value={params.formObject.language}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="pageSize">Page Size: </label>
+              <input
+                type="number"
+                size={10}
+                id="pageSize"
+                name="pageSize"
+                value={params.formObject.pageSize}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+        )}
+        <div className={currentUserIsAdmin() ? "visible" : "hidden"} style={{ border: "solid black 1px" }}>
+          {/* Extra fields */}
+          <div>
+            <label htmlFor="language">Language: </label>
+            <input
+              type="text"
+              size={10}
+              id="language"
+              name="language"
+              value={params.formObject.language}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="pageSize">Page Size: </label>
+            <input
+              type="number"
+              size={10}
+              id="pageSize"
+              name="pageSize"
+              value={params.formObject.pageSize}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <span style={{ display: "block", backgroundColor: "#eee" }}>
+          <input type="button" value="Submit" onClick={onSubmitClick} />
+        </span>
+      </form>
+    </div>
+  );
 }
