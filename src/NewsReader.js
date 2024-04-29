c8e8f90858c19c1a9935491506b2b5c8603f87ba
Rob Drouin <rob.drouin@gmail.com>
2024-04-29 15:56:26 -0400

    now gets live article list from server

--- src/NewsReader.js
+++ src/NewsReader.js
@@ -1,40 +1,59 @@
-
 import { QueryForm } from './QueryForm';
 import { Articles } from './Articles';
 import { useState, useEffect } from 'react';
-import { exampleQuery ,exampleData } from './data';
+import { exampleQuery, exampleData } from './data';
 
 export function NewsReader() {
   const [query, setQuery] = useState(exampleQuery); // latest query send to newsapi
-  const [data, setData] = useState(exampleData);   // current data returned from newsapi
+  const [data, setData] = useState(exampleData); // current data returned from newsapi
   const [queryFormObject, setQueryFormObject] = useState({ ...exampleQuery });
+  const urlNews = "/news";
 
   useEffect(() => {
     getNews(query);
-  }, [query])
+  }, [query]);
 
   function onFormSubmit(queryObject) {
     setQuery(queryObject);
   }
 
   async function getNews(queryObject) {
-    if (queryObject.q) {
-        setData(exampleData);
-    } else {
+    if (!queryObject.q) {
       setData({});
+    } else {
+      try {
+        const response = await fetch(urlNews, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(queryObject)
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const responseData = await response.json();
+        setData(responseData);
+      } catch (error) {
+        console.error('Error getting news:', error);
+        setData({});
+      }
     }
   }
 
   return (
     <div>
-      <div >
-        <section className="parent" >
+      <div>
+        <section className="parent">
           <div className="box">
             <span className='title'>Query Form</span>
             <QueryForm
               setFormObject={setQueryFormObject}
               formObject={queryFormObject}
-              submitToParent={onFormSubmit} />
+              submitToParent={onFormSubmit}
+            />
           </div>
           <div className="box">
             <span className='title'>Articles List</span>
@@ -43,5 +62,5 @@ export function NewsReader() {
         </section>
       </div>
     </div>
-  )
+  );
 }
